7ab7db3b356b845d0bf5f82ac4b23dcd1a144f33
Michael Maguire <bitbucket@michaelmaguire.ca>
2015-05-15 10:03:04 +0100

    Add initial XCode project

---
+++ asn1cpp/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  asn1cpp
+//
+//  Created by Michael Maguire on 15/05/2015.
+//  Copyright (c) 2015 Michael Maguire. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
